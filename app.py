478273620e304f3ae2b8436174154141fa93f00e
evgG <evgueniy.g@gmail.com>
2017-09-11 15:47:51 +0300

    First commit Basic connexion+sqlalchemy example

---
+++ app.py
@@ -0,0 +1,60 @@
+import connexion
+from hero import Hero, init_db
+
+db_uri = 'sqlite:///app.sqlite'
+
+
+def post_greeting(name):
+    return 'Hello {name}'.format(name=name)
+
+
+def get_greeting():
+    return dict(name='John')
+
+
+def get_all():
+    heroes = db_session.query(Hero).all()
+    # heroes = hero.Hero.query.all()
+    return [h.serialize() for h in heroes]
+
+
+def post_hero(name):
+    db_session.add(Hero(name=name))
+    db_session.commit()
+    return connexion.NoContent, 201
+
+
+def get_hero(id):
+    q = (db_session.query(Hero).
+         filter(Hero.id == id).one_or_none())
+    if q:
+        return q.serialize(), 200
+    return connexion.NoContent, 404
+
+
+def put_hero(id, name):
+    q = (db_session.query(Hero).
+         filter(Hero.id == id).one_or_none())
+    if q:
+        q.name = name
+        db_session.commit()
+        return connexion.NoContent, 201
+    return connexion.NoContent, 404
+
+
+def delete_hero(id):
+    q = (db_session.query(Hero).
+         filter(Hero.id == id).one_or_none())
+    if q:
+        db_session.query(Hero).filter(Hero.id == id).delete()
+        db_session.commit()
+        return connexion.NoContent, 204
+    return connexion.NoContent, 404
+
+
+app = connexion.FlaskApp(__name__, specification_dir='swagger/')
+# app = connexion.FlaskApp(__name__, specification_dir='swagger/',
+#                          swagger_ui=False)
+db_session = init_db(db_uri)
+app.add_api('your_api.yaml')
+app.run(port=8080)
---
+++ hero.py
@@ -0,0 +1,27 @@
+# coding=utf-8
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+def init_db(uri):
+    engine = create_engine(uri, convert_unicode=True)
+    db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False,
+                                             bind=engine))
+    Base.query = db_session.query_property()
+    Base.metadata.create_all(bind=engine)
+
+    return db_session
+
+
+class Hero(Base):
+    __tablename__ = 'heroes'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+    def serialize(self):
+        return dict(id=self.id, name=self.name,)
